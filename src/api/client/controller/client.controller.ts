1f332c6280be15b4cb303f528299725c5558680a
marat.kiyamov <marat.kiyamov@onlinesup.com>
2022-08-30 21:04:22 +0300

    master Fixed: Multer Array

--- src/api/client/controller/client.controller.ts
+++ src/api/client/controller/client.controller.ts
@@ -2,11 +2,11 @@ import {Body, Controller, HttpException, Inject, Post, Response, UploadedFiles, 
 import {ApiConsumes} from '@nestjs/swagger';
 import {FilesInterceptor} from '@nestjs/platform-express';
 import {ClientService} from '../service/client.service';
-import * as fs from 'fs';
-import * as csv from 'fast-csv';
-import * as path from 'path';
 import {ExtensionService} from '../../extension/service/extension.service';
 import {BodyDto} from '../../dto/body.dto';
+import fs from 'fs';
+import path from 'path';
+import * as csv from 'fast-csv';
 
 @Controller('client')
 export class ClientController {
@@ -35,8 +35,7 @@ export class ClientController {
         return res.json({message: `File is empty`});
       }
       const existSites = [];
-      await fs
-        .createReadStream(path.resolve(__dirname, '../../../..', 'uploads', `${files[0].filename}`))
+      fs.createReadStream(path.resolve(__dirname, '../../../..', 'uploads', `${files[0].filename}`))
         .pipe(csv.parse())
         .on('error', (e) => {
           return res.json({message: `Something gone wrong: ${e.message}`});
@@ -48,6 +47,7 @@ export class ClientController {
           ) {
             throw new HttpException('Bad Request', 400);
           }
+          console.log(data);
           const site = await this.extensionService.getSite(data[0]);
           if (site.rows[0].fqdn === new URL(data[0]).hostname) {
             existSites.push(new URL(data[0]).hostname);
@@ -58,13 +58,12 @@ export class ClientController {
         .on('end', async (rowCount: number) => {
           await fs.rmSync(path.resolve(__dirname, '../../../..', 'uploads', `${files[0].filename}`));
           console.log(`Parsed ${rowCount} rows`);
+          return {
+            existsSites: existSites,
+            type: 'file',
+            message: `OK, parsed ${rowCount} rows`,
+          };
         });
-
-      console.log('existSites', existSites);
-      return res.json({
-        existSites,
-        message: 'OK',
-      });
     } else if (body.domains) {
       const existSites = [];
       const result = body.domains.map(async (domain) => {
@@ -75,14 +74,20 @@ export class ClientController {
           throw new HttpException('Bad Request', 400);
         }
         const site = await this.extensionService.getSite(domain);
-        if (site.rows[0].fqdn) {
-          existSites.push(new URL(domain).hostname);
-        } else {
+        if (!site.rows.length) {
           await this.extensionService.createSite(domain, accountId);
+        } else {
+          existSites.push(new URL(domain).hostname);
         }
       });
-      if (!result) throw new HttpException('Server Error', 500);
-      return res.json({existSites, message: 'OK'});
+      Promise.all(result).then(() => {
+        if (!result) throw new HttpException('Server Error', 500);
+        return res.json({
+          existSites,
+          type: 'array',
+          message: 'OK',
+        });
+      });
     }
   }
 }
