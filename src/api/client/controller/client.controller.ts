e60f5eb7742637ce85634c8de738ece1159108f0
nikita.matyash <nikita.matyash@onlinesup.com>
2022-09-05 16:27:01 +0300

    Change uploadfiles

--- src/api/client/controller/client.controller.ts
+++ src/api/client/controller/client.controller.ts
@@ -13,9 +13,6 @@ import {FilesInterceptor} from '@nestjs/platform-express';
 import {ClientService} from '../service/client.service';
 import {ExtensionService} from '../../extension/service/extension.service';
 import {BodyDto} from '../../dto/body.dto';
-import * as fs from 'fs';
-import * as es from 'event-stream';
-import * as path from 'path';
 
 @Controller('client')
 export class ClientController {
@@ -39,133 +36,17 @@ export class ClientController {
     }
 
     if (files) {
-      if (!files.length) {
-        return res.json({message: `File is empty`});
-      }
-      const existSites = [];
-      const badSites = [];
-      const goodSites = [];
-      let lineNr = 0;
-      const s = fs
-        .createReadStream(path.join(__dirname, '../../../..', 'uploads', `${files[0].filename}`))
-        .pipe(es.split())
-        .pipe(
-          es.mapSync(async (domain) => {
-            s.pause();
-            lineNr += 1;
-            // console.log(domain);
-            const origin = this.parseDomain(domain);
-            if (origin) {
-              // console.log(`line: ${lineNr}, data: ${data}, time: ${new Date().toLocaleString('ru-RU')}`);
-              const site = await this.extensionService.getSite(origin);
-              if (site.rows[0] && site.rows[0].fqdn === new URL(origin).hostname) {
-                existSites.push({
-                  numberOfString: lineNr,
-                  origin: domain,
-                });
-              } else {
-                // console.log(`line: ${lineNr}, data: ${data}, time: ${new Date().toLocaleString('ru-RU')}`);
-                await this.extensionService.createSite(origin, accountId, query.suppress, query.cabinet);
-                goodSites.push({
-                  numberOfString: lineNr,
-                  origin: domain,
-                });
-              }
-              s.resume();
-            } else {
-              // console.log(`line: ${lineNr}, data: ${domain}, time: ${new Date().toLocaleString('ru-RU')}`);
-              badSites.push({
-                numberOfString: lineNr,
-                origin: domain,
-              });
-              s.resume();
-            }
-          }),
-        )
-        .on('end', async () => {
-          await fs.rmSync(path.resolve(__dirname, '../../../..', 'uploads', `${files[0].filename}`));
-          return res.json({
-            existsSites: {
-              existSitesList: existSites,
-              existSitesCount: existSites.length,
-            },
-            badSites: {
-              badSitesList: badSites,
-              badSitesCount: badSites.length,
-            },
-            goodSites: {
-              goodSitesList: goodSites,
-              goodSitesCount: goodSites.length,
-            },
-            type: 'file',
-            message: `OK, parsed`,
-          });
-        });
+      const result = this.clientService.uploadFromFile(files, res.locals.accountId, query.suppress, query.cabinet);
+      res.json(result);
     }
     if (body.domains) {
-      const existSites = [];
-      const badSites = [];
-      const goodSites = [];
-      let lineNr = 0;
-      const result = body.domains.map(async (domain) => {
-        lineNr += 1;
-        const origin = this.parseDomain(domain);
-        if (origin) {
-          const site = await this.extensionService.getSite(origin);
-
-          if (site.rows[0] && site.rows[0].fqdn === new URL(origin).hostname) {
-            // console.log(`line: ${lineNr}, data: ${domain}, time: ${new Date().toLocaleString('ru-RU')}`);
-            existSites.push({
-              numberOfString: lineNr,
-              origin: domain,
-            });
-          } else {
-            await this.extensionService.createSite(origin, accountId, query.suppress, query.cabinet);
-            // console.log(`line: ${lineNr}, data: ${domain}, time: ${new Date().toLocaleString('ru-RU')}`);
-            goodSites.push({
-              numberOfString: lineNr,
-              origin: domain,
-            });
-          }
-        } else {
-          // console.log(`line: ${lineNr}, data: ${domain}, time: ${new Date().toLocaleString('ru-RU')}`);
-          badSites.push({
-            numberOfString: lineNr,
-            origin: domain,
-          });
-        }
-      });
-      Promise.all(result).then(() => {
-        if (!result) throw new HttpException('Server Error', 500);
-        return res.json({
-          existsSites: {
-            existSitesList: existSites,
-            existSitesCount: existSites.length,
-          },
-          badSites: {
-            badSitesList: badSites,
-            badSitesCount: badSites.length,
-          },
-          goodSites: {
-            goodSitesList: goodSites,
-            goodSitesCount: goodSites.length,
-          },
-          type: 'array',
-          message: 'OK',
-        });
-      });
-    }
-  }
-
-  private parseDomain(value) {
-    try {
-      // if (value.startsWith('http://') || value.startsWith('https://')) {
-      //   return new URL(String(value).toLowerCase().trim()).origin;
-      // } else {
-        return new URL(`http://${String(value).toLowerCase().trim()}`).origin;
-      // }
-    } catch (e) {
-      return '';
+      const result = this.clientService.uploadFromBody(
+        body.domains,
+        res.locals.accountId,
+        query.suppress,
+        query.cabinet,
+      );
+      res.json(result);
     }
   }
 }
--- src/api/client/service/client.service.ts
+++ src/api/client/service/client.service.ts
@@ -1,13 +1,140 @@
-import {Inject} from '@nestjs/common';
+import {HttpException, Inject} from '@nestjs/common';
 import Redis from 'ioredis';
+import fs from 'fs';
+import path from 'path';
+import * as es from 'event-stream';
+import {PoolClient} from 'pg';
 
 export class ClientService {
   constructor(
     @Inject('REDIS_CLIENT')
     private readonly redis: Redis,
+    @Inject('PG_CONNECTION')
+    private readonly pg: PoolClient,
   ) {}
 
-  public async uploadData(data, stream) {
-    console.log(stream);
+  public async uploadFromFile(files, accountId, suppress, cabinet) {
+    if (!files.length) {
+      return {message: `File is empty`};
+    }
+    const existSites = [];
+    const badSites = [];
+    const goodSites = [];
+    let lineNr = 0;
+    const s = fs
+      .createReadStream(path.join(__dirname, '../../../..', 'uploads', `${files[0].filename}`))
+      .pipe(es.split())
+      .pipe(
+        es.mapSync(async (line) => {
+          s.pause();
+          lineNr += 1;
+          const hostname = this.parseDomain(line);
+          if (hostname) {
+            const site = await this.pg.query('SELECT * FROM sites WHERE fqdn = $1::text', [hostname]);
+            if (site.rows[0] && site.rows[0].fqdn === hostname) {
+              existSites.push({
+                numberOfString: lineNr,
+                origin: line,
+              });
+            } else {
+              await this.pg.query(
+                'INSERT INTO sites (fqdn, created_by, suppress, cabinet) VALUES ($1::text, $2::integer, $3::boolean, $4::boolean)',
+                [hostname, accountId, suppress, cabinet],
+              );
+              goodSites.push({
+                numberOfString: lineNr,
+                origin: line,
+              });
+            }
+            s.resume();
+          } else {
+            badSites.push({
+              numberOfString: lineNr,
+              origin: line,
+            });
+            s.resume();
+          }
+        }),
+      )
+      .on('end', async () => {
+        await fs.rmSync(path.resolve(__dirname, '../../../..', 'uploads', `${files[0].filename}`));
+        return {
+          existsSites: {
+            existSitesList: existSites,
+            existSitesCount: existSites.length,
+          },
+          badSites: {
+            badSitesList: badSites,
+            badSitesCount: badSites.length,
+          },
+          goodSites: {
+            goodSitesList: goodSites,
+            goodSitesCount: goodSites.length,
+          },
+          type: 'file',
+          message: `OK, parsed`,
+        };
+      });
+  }
+
+  public async uploadFromBody(domainList, accountId, suppress, cabinet) {
+    const existSites = [];
+    const badSites = [];
+    const goodSites = [];
+    let lineNr = 0;
+    const result = domainList.map(async (domain) => {
+      lineNr += 1;
+      const hostname = this.parseDomain(domain);
+      if (hostname) {
+        const site = await this.pg.query('SELECT * FROM sites WHERE fqdn = $1::text', [hostname]);
+        if (site.rows[0] && site.rows[0].fqdn === hostname) {
+          existSites.push({
+            numberOfString: lineNr,
+            origin: domain,
+          });
+        } else {
+          await this.pg.query(
+            'INSERT INTO sites (fqdn, created_by, suppress, cabinet) VALUES ($1::text, $2::integer, $3::boolean, $4::boolean)',
+            [hostname, accountId, suppress, cabinet],
+          );
+          goodSites.push({
+            numberOfString: lineNr,
+            origin: domain,
+          });
+        }
+      } else {
+        badSites.push({
+          numberOfString: lineNr,
+          origin: domain,
+        });
+      }
+    });
+    Promise.all(result).then(() => {
+      if (!result) throw new HttpException('Server Error', 500);
+      return {
+        existsSites: {
+          existSitesList: existSites,
+          existSitesCount: existSites.length,
+        },
+        badSites: {
+          badSitesList: badSites,
+          badSitesCount: badSites.length,
+        },
+        goodSites: {
+          goodSitesList: goodSites,
+          goodSitesCount: goodSites.length,
+        },
+        type: 'array',
+        message: 'OK',
+      };
+    });
+  }
+
+  private parseDomain(value) {
+    try {
+      return new URL(`http://${String(value).toLowerCase().trim()}`).hostname;
+    } catch (e) {
+      return '';
+    }
   }
 }
